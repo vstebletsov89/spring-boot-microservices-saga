1f0ed077e5d74d32777eafe8bfa8c268fec74bb8
vs891026 <vyacheslav.stebletsov@broadcom.com>
2025-04-14 13:12:18 +0200

    init

--- ticket-service/src/main/java/ru/otus/ticketservice/TicketServiceApplication.java
+++ ticket-service/src/main/java/ru/otus/ticketservice/TicketServiceApplication.java
@@ -2,7 +2,11 @@ package ru.otus.ticketservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@EnableKafkaStreams
 @SpringBootApplication
 public class TicketServiceApplication {
 
