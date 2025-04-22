df55180aa130bf3cb0919eeaf9ed511e2112b4b6
Viacheslav Stebletsov <vstebletsov89@github.com>
2025-04-22 08:44:03 +0200

    init

---
+++ ticket-service/src/test/java/ru/otus/ticket/config/JacksonConfig.java
@@ -0,0 +1,19 @@
+package ru.otus.ticket.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@TestConfiguration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
+    }
+}
