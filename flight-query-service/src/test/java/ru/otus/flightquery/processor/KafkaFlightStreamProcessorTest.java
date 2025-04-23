ccade9dce17243d1fa2a40b947b2363785b193f3
vs891026 <vyacheslav.stebletsov@broadcom.com>
2025-04-23 17:25:30 +0200

    init

---
+++ flight-query-service/src/test/java/ru/otus/flightquery/processor/KafkaFlightStreamProcessorTest.java
@@ -0,0 +1,93 @@
+package ru.otus.flightquery.processor;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import ru.otus.common.event.FlightCreatedEvent;
+import ru.otus.flightquery.publisher.DltPublisher;
+import ru.otus.flightquery.service.FlightSyncService;
+
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+
+@SpringBootTest
+@EmbeddedKafka(partitions = 1, topics = {"test-topic", "test-dlt"})
+@TestPropertySource(properties = {
+        "app.kafka.topic.flights=test-topic",
+        "app.kafka.topic.dlt=test-dlt",
+        "spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}"
+})
+class KafkaFlightStreamProcessorTest {
+
+    @Autowired
+    private EmbeddedKafkaBroker embeddedKafka;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private FlightSyncService flightSyncService;
+
+    @MockitoBean
+    private DltPublisher dltPublisher;
+
+    private KafkaTemplate<String, String> createKafkaTemplate() {
+        Map<String, Object> producerProps = KafkaTestUtils.producerProps(embeddedKafka);
+        return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(producerProps, new StringSerializer(), new StringSerializer()));
+    }
+
+    @Test
+    void shouldProcessFlightCreatedEvent() throws Exception {
+
+        ZonedDateTime departureTime = ZonedDateTime.parse("2025-04-23T15:07:15.934709900Z");
+        ZonedDateTime arrivalTime = departureTime.plusHours(8);
+
+        FlightCreatedEvent event = new FlightCreatedEvent(
+                "FL123", "SVO", "JFK",
+                ru.otus.common.enums.FlightStatus.SCHEDULED,
+                departureTime,
+                arrivalTime,
+                new BigDecimal("999.99"),
+                180, 0, new BigDecimal("10.00"));
+
+        String payload = objectMapper.writeValueAsString(event)
+                .replaceFirst("\\{", "{\"type\":\"FlightCreatedEvent\",");
+        KafkaTemplate<String, String> template = createKafkaTemplate();
+        template.send(new ProducerRecord<>("test-topic", "FL123", payload));
+
+        Awaitility.await().atMost(5, TimeUnit.SECONDS).untilAsserted(() -> {
+            verify(flightSyncService).handleCreated(eq(event));
+            verifyNoInteractions(dltPublisher);
+        });
+    }
+
+    @Test
+    void shouldSendToDltOnDeserializationError() {
+        String invalidJson = "{ \"type\":\"FlightCreatedEvent\", \"bad\": }";
+        KafkaTemplate<String, String> template = createKafkaTemplate();
+        template.send("test-topic", "invalid", invalidJson);
+
+        Awaitility.await().atMost(5, TimeUnit.SECONDS).untilAsserted(() -> {
+            verify(dltPublisher).publish(eq("test-dlt"), eq("invalid"), eq(invalidJson));
+            verifyNoInteractions(flightSyncService);
+        });
+    }
+}
