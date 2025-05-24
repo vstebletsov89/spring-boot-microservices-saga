a55d5a481947d32c5ee7985de0017387651ce544
vstebletsov89 <stebletsov-vs@yandex.ru>
2025-05-24 19:21:10 +0200

    wip

--- reservation-service/src/main/java/ru/otus/reservation/ReservationServiceApplication.java
+++ reservation-service/src/main/java/ru/otus/reservation/ReservationServiceApplication.java
@@ -3,8 +3,10 @@ package ru.otus.reservation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableKafkaStreams
+@EnableScheduling
 @SpringBootApplication
 public class ReservationServiceApplication {
 
