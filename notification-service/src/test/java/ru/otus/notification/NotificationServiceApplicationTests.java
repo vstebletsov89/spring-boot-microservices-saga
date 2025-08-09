81f41c9fff008221edd7ce5cbb9910c1205fa7db
vstebletsov89 <stebletsov-vs@yandex.ru>
2025-08-09 20:40:55 +0200

    wip

--- notification-service/src/test/java/ru/otus/notification/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/ru/otus/notification/NotificationServiceApplicationTests.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class NotificationServiceApplicationTests {
 
-    //TODO: add postgresql/service to docker compose
     //TODO: unit/int tests
 
     @Test
